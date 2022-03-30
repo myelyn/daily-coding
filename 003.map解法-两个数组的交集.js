1dce8b22f0aeec5ac4b43aad6bc84d87d6b67f04
myelyn <myelyn@163.com>
2022-03-30 13:57:45 +0800

    add a comment to 003

--- 003.map解法-两个数组的交集.js
+++ 003.map解法-两个数组的交集.js
@@ -1,5 +1,6 @@
 // 题目：给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
 
+// 思路：分别对两个数组创建map，map的key为该数组中出现的数字，value为该数字在数组中出现的次数。然后遍历其中一个map，对每一个key，取出它在另一个map中的value，两个value比较，最小值则为在两个数组中共同出现的次数min，将当前key push到result数组中min次。
 const intersect = (arr1, arr2) => {
 	const creatMap = (arr) => {
 		const map = new Map()
